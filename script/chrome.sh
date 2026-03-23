1d13f12f4f8dca91d200e1abf7ccb7c193b88804
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-03-23 19:07:30 +0900

    update

--- script/chrome.sh
+++ script/chrome.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+set -euo pipefail
+
 google-chrome --no-sandbox --disable-dev-shm-usage --no-first-run --no-default-browser-check --hide-crash-restore-bubble >> "${PATH_ROOT}${MS_M_PATH_LOG}chrome.log" 2>&1 &
